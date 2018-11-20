0c7dce10021d9c17e5be83b2da2c64b3c3e55e10
Amitabh Singh <amitabh.k.singh@kp.org>
2018-11-20 09:20:14 +0530

    Added Angular Services

---
+++ spring-boot-app/src/main/java/com/boot/springbootapp/SpringBootApp.java
@@ -0,0 +1,11 @@
+package com.boot.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp.class, args);
+	}
+}
