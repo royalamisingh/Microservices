9f0ef6fc59dac083a1611e95ae030b7e149fd3d3
Amitabh Singh <amitabh.k.singh@kp.org>
2018-11-23 18:00:41 +0530

    Entity Scan

--- spring-boot-app/src/main/java/com/boot/springbootapp/SpringBootApp.java
+++ spring-boot-app/src/main/java/com/boot/springbootapp/SpringBootApp.java
@@ -1,8 +1,10 @@
 package com.boot.springbootapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
+@EntityScan("com.boot.model")
 public class SpringBootApp {
 
 	public static void main(String[] args) {
--- spring-boot-app/src/main/java/com/boot/springbootapp/repository/ShipWreckRepository.java
+++ spring-boot-app/src/main/java/com/boot/springbootapp/repository/ShipWreckRepository.java
@@ -1,9 +1,10 @@
 package com.boot.springbootapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.boot.model.Shipwreck;
 
+@Repository
 public interface ShipWreckRepository extends JpaRepository<Shipwreck, Long>{
 
 }
