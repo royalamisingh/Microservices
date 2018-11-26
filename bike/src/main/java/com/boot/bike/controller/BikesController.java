9f37e77941654a44fe795351e6d637633d85ec66
Amitabh Singh <amitabh.k.singh@kp.org>
2018-11-26 15:22:16 +0530

    This is a Bike Registration app using Angular and Spring Boot

---
+++ bike/src/main/java/com/boot/bike/controller/BikesController.java
@@ -0,0 +1,39 @@
+package com.boot.bike.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boot.bike.models.Bike;
+
+@RestController
+@RequestMapping("/api/v1/bikes")
+public class BikesController {
+	
+	@GetMapping
+	public List<Bike> list(){
+		
+		List<Bike> list = new ArrayList<>();
+		return list;
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.OK)
+	public void create(@RequestBody Bike bike) {
+		
+	}
+	
+	@GetMapping("/{id}")
+	public Bike get(@PathVariable("id") long id) {
+		return new Bike();
+	}
+
+}
